4c7319e4b19e2b7efd442095abd3746ee822f627
Mila Tran <mila.tran@contemi.com>
2023-01-16 18:20:51 +0700

    keys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import * as keys from './global-keys.json';
+const obj = require('./global-keys.json');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'untitled11';
+
+  ngOnInit() {
+    // console.log(obj);
+    let result = {} as any;
+    for(var key in obj){
+      if(Array.isArray(result[obj[key]])){
+        result[obj[key]].push(key);
+      } else {
+        result[obj[key]] = [key];
+      }
+    }
+
+    for(var key2 in result){
+      if(result[key2].length < 2){
+        delete result[key2];
+      }
+    }
+
+    // console.log(JSON.stringify(result));
+    function sortObject(obj: object){
+      return Object.entries(obj).sort((a,b) => b[1].length-a[1].length).filter(value => value[1].length > 10);
+    }
+
+    // console.log(result);
+    console.log(sortObject(result));
+  }
+
 }
